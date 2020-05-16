6c8a111fec147c6bf8db44f4288c81e6206ccaed
Alok8 <40895847+Alok8@users.noreply.github.com>
2020-05-16 14:08:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def predict():
 	return render_template('index.html',prediction_text="Number of Weekly Rides should be {}".format(int(output)))
 
 if __name__=='__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0',port=8080)
